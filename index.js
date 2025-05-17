fe2c0634ad55d6abed75a0061b5ae12e15d4fcbe
alcendol <gubranaldisar@gmail.com>
2025-05-17 14:42:45 +0700

    Simple

--- index.js
+++ index.js
@@ -1,15 +1,43 @@
-const {ApolloServer, gql} = require("apollo-server");
+const { ApolloServer, gql } = require("apollo-server");
 
 const typeDefs = gql`
+  type User {
+    firstName: String!
+    lastName: String!
+    email: String!
+  }
+
   type Query {
     hello: String!
     randomNumber: Int!
+    queryUsers: [User]!
   }
-`
+
+  type Mutation {
+    addUser(firstName: String!, lastName: String!, email: String!): User!
+  }
+`;
+
+const users = [
+  {
+    firstName: "Aldisar",
+    lastName: "Gibran",
+    email: "aldisarg@gmail.com",
+  },
+];
+
 const resolvers = {
   Query: {
     hello: () => "Hello world!",
-    randomNumber: () => Math.round(Math.random()*10),
+    randomNumber: () => Math.round(Math.random() * 10),
+    queryUsers: () => users,
+  },
+
+  Mutation: {
+    addUser: (parent, args) => {
+      users.push(args);
+      return args;
+    },
   },
 };
 
@@ -18,5 +46,6 @@ const server = new ApolloServer({
   resolvers,
 });
 
-server.listen({port:8080}).then(({ url}) => console.log(`GraphQL server running at ${url}`));
-
+server
+  .listen({ port: 8080 })
+  .then(({ url }) => console.log(`GraphQL server running at ${url}`));
